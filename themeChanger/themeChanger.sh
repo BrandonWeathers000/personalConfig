22100cf81f30e2b12855ee482c84e64ad1246829
Brandon Weathers <brandon.r.weathers@gmail.com>
2026-08-05 01:04:54 -0400

    proper waybar hotswapping

--- themeChanger/themeChanger.sh
+++ themeChanger/themeChanger.sh
@@ -10,42 +10,23 @@ brightnessValue(){
     awk -v r="$r" -v g="$g" -v b="$b" 'BEGIN { print 0.2126*r + 0.7152*g + 0.0722*b }'
 }
 
-# findingColors(){
-#     # Getting colors
-#     colorOne=$(magick $1 -colors 2 -unique-colors txt:- | awk 'NR > 1 { print $3 }' | head -n 1 | sed 's/#//g')
-#     colorTwo=$(magick $1 -colors 2 -unique-colors txt:- | awk 'NR > 1 { print $3 }' | tail -n 1 | sed 's/#//g')
-
-#     colorOneBright=$(brightnessValue $colorOne)
-#     colorTwoBright=$(brightnessValue $colorTwo)
-
-#     if awk "BEGIN { exit !($colorTwoBright > $colorOneBright) }"; then
-#         temp=$colorOne
-#         colorOne=$colorTwo
-#         colorTwo=$temp
-
-#         echo "Testing"
-#     fi
-
-#     # echo "Color ONE is: $colorOne and is $colorOneBright bright"
-#     # echo "Color TWO is: $colorTwo and is $colorTwoBright bright"
-
-#     # if awk "BEGIN { exit !($colorOneBright < 10.0) }"; then
-#     #     echo "Manual overide to white color one!"
-
-#     #     riverctl border-color-focused "0x$colorTwo"
-#     #     riverctl border-color-unfocused 0xFFFFFF
-
-#     # elif awk "BEGIN { exit !($colorTwoBright < 10.0) }"; then
-#     #     echo "Manual overide to white color two!"
-
-#     #     riverctl border-color-focused "0x$colorOne"
-#     #     riverctl border-color-unfocused 0xFFFFFF
-#     # else
-# }
-
 wallpaperChange(){
     killall swaybg 
     swaybg -m fill -i $1
+}
+
+waybarColors(){
+    killall waybar
+
+    newColor=$1
+    cssFile=~/.config/waybar/style.css
+
+    oldColor=$(sed -n '11p' $cssFile | awk '{print $2}' | sed 's/#\|;//g')
+    echo "The old color is: $oldColor"
+
+    sed -i "s/$oldColor/$newColor/g" $cssFile
+
+    waybar & 
 }
 
 ###################
@@ -61,9 +42,6 @@ mapfile -t colors < <(magick $wallpaper -colors 2 -unique-colors txt:- | awk 'NR
 colorOne=${colors[0]}
 colorTwo=${colors[1]}
 
-echo "Color ONE is: $colorOne"
-echo "Color TWO is: $colorTwo"
-
 colorOneBright=$(brightnessValue $colorOne)
 colorTwoBright=$(brightnessValue $colorTwo)
 
@@ -75,9 +53,28 @@ if awk "BEGIN { exit !($colorTwoBright > $colorOneBright) }"; then
     echo "Testing"
 fi
 
+echo "Color ONE is: $colorOne and is $colorOneBright bright"
+echo "Color TWO is: $colorTwo and is $colorTwoBright bright"
+
+# if awk "BEGIN { exit !($colorOneBright < 10.0) }"; then
+#     echo "Manual overide to white color one!"
+
+#     riverctl border-color-focused "0x$colorTwo"
+#     riverctl border-color-unfocused 0xFFFFFF
+
+# elif awk "BEGIN { exit !($colorTwoBright < 10.0) }"; then
+#     echo "Manual overide to white color two!"
+
+#     riverctl border-color-focused "0x$colorOne"
+#     riverctl border-color-unfocused 0xFFFFFF
+# else
+
 # Setting window borders
 riverctl border-color-focused "0x$colorOne"
 riverctl border-color-unfocused "0x$colorTwo"
+
+# Changing waybar colors
+waybarColors $colorOne
 
 # Changing wallpaper
 wallpaperChange $wallpaper
